73b53c9cc697642499c57a30336c9db7537dd723
Eric Gazoni <eric.gazoni@adimian.com>
2015-05-22 15:35:04 +0200

    remove change to pythonpath

--- build-coverage.sh
+++ build-coverage.sh
@@ -4,7 +4,6 @@ source .ve/bin/activate
 echo ">>> installing dependencies"
 pip install pytest-cov lxml jdcal
 echo ">>> perform coverage tests"
-export PYTHONPATH=$PYTHONPATH:/source/openpyxl
 mkdir -p shippable/codecoverage
 py.test --cov openpyxl --cov-report xml --junit-xml=shippable/codecoverage/results.xml
 py.test --cov openpyxl --cov-report html
